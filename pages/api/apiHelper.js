35a37fd6fe00fa97e4f96735243814ccbb2b3bff
Sean Menezes <sdmenezes@uwaterloo.ca>
2022-01-20 17:02:52 -0500

    updated api endpoint

--- pages/api/apiHelper.js
+++ pages/api/apiHelper.js
@@ -1,5 +1,5 @@
-// export const BASE_URL = process.env.REACT_APP_API_ENDPOINT;
-export const BASE_URL = "http://localhost:5000";
+// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "https://wordle-clone-api.herokuapp.com";
 
 export const get = async (endpoint) => {
     let response = await fetch(`${BASE_URL}/${endpoint}`, {
