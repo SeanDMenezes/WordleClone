d4cfb957ff89584e43c454e2c8ef66e02b95ecdc
Sean Menezes <sdmenezes@uwaterloo.ca>
2022-11-23 00:54:06 -0500

    move backend to vercel

--- pages/api/apiHelper.js
+++ pages/api/apiHelper.js
@@ -1,5 +1,5 @@
 // export const BASE_URL = "http://localhost:5000";
-export const BASE_URL = "https://wordle-clone-api.herokuapp.com";
+export const BASE_URL = "https://wordle-clone-api-tau.vercel.app";
 
 export const get = async (endpoint) => {
     let response = await fetch(`${BASE_URL}/${endpoint}`, {
